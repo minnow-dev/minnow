91f4217e84b80179748ac0febe6a4465ddfcc1c8
leekt <leekt216@gmail.com>
2020-12-27 16:04:00 +0900

    separated time file

--- index.ts
+++ index.ts
@@ -1,19 +1,5 @@
-import hre from "hardhat";
-import "@nomiclabs/hardhat-ethers";
-import { providers, Contract, Signer, BigNumber } from "ethers";
+import { time } from "./src/time";
 
-export async function increase(seconds: number) {
-  const ethers = hre.ethers;
-  const signers = await ethers.getSigners();
-  const signer = signers[0];
-  await (signer.provider as providers.JsonRpcProvider).send("evm_increaseTime", [seconds]);
-  await (signer.provider as providers.JsonRpcProvider).send("evm_mine", []);
-}
-
-export async function getTimestamp() : Promise<BigNumber> {
-  const ethers = hre.ethers;
-  const signers = await ethers.getSigners();
-  const signer = signers[0];
-  const res = await (signer.provider as providers.JsonRpcProvider).send("eth_getBlockByNumber", ["latest", false]);
-  return BigNumber.from(res.timestamp);
+export {
+  time
 }
